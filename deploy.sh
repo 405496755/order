a3bfcf69c7b5e3f626c5188234cced0ea53ff00e
ly <405496755@qq.com>
2022-05-19 02:57:56 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ killTomcat()
     fi
 }
 cd $PROJ_PATH/order
-mvn clean install
+mvn clean
 
 # 停tomcat
 killTomcat
@@ -30,6 +30,8 @@ killTomcat
 rm -rf $TOMCAT_APP_PATH/webapps/ROOT
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
+
+mvn install
 
 # 复制新的工程
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
